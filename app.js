4ed9e0eaa4b23440c277affd22fc9327f7d903ea
mauritio98 <57999694+mauritio98@users.noreply.github.com>
2022-02-25 00:32:43 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,94 +1,50 @@
-import data_movie from './share/data-movies.js';
+import express from 'express';
 import moviesController from './controllers/moviesController.js';
-import { new_movie } from './data/newMovie.js';
-import { new_movie_update } from './data/newMovie.js';
+import data_movie from './share/data-movies.js'
 
-//CRUD
-// Create / Read / Update / Delete
+const app = express();
 
-let allMovies = function (data_movie) {
-    moviesController.getAllMovies(data_movie);
-    console.log("\n__________________Devuelve todas las peliculas (1pto)____________________\n")
-    data_movie.res.forEach((e) => console.log(e));
-}
+app.use(express.json());
 
-
-let getMovieId = function (data_movie) {
+// Película con id:2 (1pto)
+app.get('/movies/:id', (req,res)=>{
+    data_movie.req = { id: req.params.id };
     moviesController.getMovieById(data_movie);
-    console.log("\n__________________Película con id:2 (1pto) ____________________\n")
-    data_movie.res.forEach((e) => console.log(e));
-}
+    res.send(data_movie.res)
+})
+// Eliminar la película con id:2 (1pto)
+app.delete('/movies/:id', (req,res)=>{
+    data_movie.req = { id: req.params.id };
+    moviesController.removeMovie(data_movie)
+    res.send(data_movie.res)
+})
+// Añade una nueva película new_movie
+app.post('/movies/add', (req,res)=>{
+    data_movie.req = req.body
+    moviesController.createMovie(data_movie)
+    res.send(data_movie.res)
+})
+//Modifica la película con id:3
+app.put('/movies/update', (req,res)=>{
+    data_movie.req = req.body
+    moviesController.updateMovie(data_movie)
+    res.send(data_movie.res)
+})
 
-let createMovie = function (data_movie) {
-    moviesController.createMovie(data_movie);
-    console.log("\n__________________ Añade una nueva película (1pto) ____________________\n")
-    data_movie.res.forEach((e) => console.log(e));
-}
+//Devuelve todas las películas
+// app.get('/movies/allfilms'), (req,res)=>{
+//     moviesController.getAllMovies(data_movie)
+//     res.send(data_movie.res)
+// }
 
-let removeMovie = function (data_movie) {
-    moviesController.removeMovie(data_movie);
-    console.log("\n__________________Eliminar la película con id:2 (1pto) ____________________\n")
-    data_movie.res.forEach((e) => console.log(e));
-}
-
-let updateMovie = function (data_movie) {
-    moviesController.updateMovie(data_movie);
-    console.log("\n__________________Modifica la película con id:3 (1pto) ____________________\n")
-    data_movie.res.forEach((e) => console.log(e));
-}
-
-
-let getMovieBy = function (data_movie) {
-    moviesController.getMovieBy(data_movie);
-    console.log("\n_________________Todas las peliculas del año 1994 (1pto) ____________________\n")
-    data_movie.res.forEach((e) => console.log(e));
-}
-
-let addActors = function (data_movie) {
-    moviesController.addActors(data_movie);
-    console.log("\n_________________Añadir un actor a la pelicula id:4 (1pto)____________________\n")
-    data_movie.res.forEach((e) => console.log(e));
-}
-
-
-try {
-
-    // Película con id:2 (1pto)
-    data_movie.req = { id: 2 };
-    getMovieId(data_movie);
-
-    // Eliminar la película con id:2 (1pto)
-    data_movie.req = { id: 2 };
-    removeMovie(data_movie);
-
-    // Añade una nueva película (1pto)
-    data_movie.req = new_movie;
-    createMovie(data_movie);
-
-    // Modifica la película con id:3 (1pto)
-    data_movie.req = new_movie_update;
-    updateMovie(data_movie);
-
-    //Devuelve todas las peliculas (1pto)
-    allMovies(data_movie);
-
-    //Todas las peliculas del año 1994 (1pto)
-    data_movie.req = { key: "year", value: 1994 };
-    getMovieBy(data_movie);
-
-    //Añadir un actor a la pelicula id:4 (1pto)
-    data_movie.req = { id: 4, value: 'Ricard el profe' };
-    addActors(data_movie);
-
-    // Película con id:20  (1pto)
-    data_movie.req = { id: 20 };
-    getMovieId(data_movie);
+//Todas las peliculas del año 1994
+app.get('/movies/year', (req,res)=>{
+    const movies_year = req.body 
+    data_movie.req = movies_year
+    moviesController.getMovieBy(data_movie)
+    res.send(data_movie.res)
+})
+//Añadir un actor a la pelicula
 
 
-} catch (error) {
-    console.log(error.message);
-}
-
-
-
-
+export default app
